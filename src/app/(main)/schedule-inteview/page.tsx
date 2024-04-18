b72ea93570114f1b04893ee734f3f3f0106d13c5
Hossein Rafiei <nh.ra1802@outlook.com>
2024-04-18 04:47:25 +0330

    27:Interview step 2 UI. Add helper func. Add react query for manage api.

--- src/app/(main)/schedule-inteview/page.tsx
+++ src/app/(main)/schedule-inteview/page.tsx
@@ -3,21 +3,23 @@ import {useState} from "react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import MobileStepper from '@mui/material/MobileStepper';
-import {AsyncPaginate} from "react-select-async-paginate";
 import Link from "next/link";
+import ChooseMentor from "@/containers/scheduleInteview/ChooseMentor";
+
 const ScheduleInterview = () => {
     const [activeStep, setActiveStep] = useState(1);
-   const renderStepperTitle = () => {
-        if (activeStep === 2) {
+    const renderStepperTitle = () => {
+        if (activeStep === 1) {
             return "Choose a mentor and schedule an interview"
-        } else if (activeStep === 3) {
+        } else if (activeStep === 2) {
             return "Finalize the interview session"
 
-        } else if (activeStep === 4) {
+            } else if (activeStep === 3) {
             return "Confirmation"
         }
         return "What are you looking for?"
     }
+
     async function loadOptions() {
         const response = await fetch(`https://jsonplaceholder.typicode.com/todos/1`);
         const responseJSON = await response.json();
@@ -28,65 +30,39 @@ const ScheduleInterview = () => {
         };
     }
 
+    const renderCurrentStepComponent = () => {
+        if (activeStep === 1) {
+            return <ChooseMentor/>
+        }
+    }
     return (
         <>
             <Header/>
-            <div className='bg-white container text-center flex flex-col gap-y-9 items-center'>
+            <div className='bg-white p-3 sm:container text-center flex flex-col gap-y-9 items-center'>
                 <p className='text-black text-2xl mt-9'>{renderStepperTitle()}</p>
-                    <div className='w-full flex items-center justify-center'>
-                        <MobileStepper
-                            variant="progress"
-                            steps={4}
-                            position="static"
-                            activeStep={activeStep}
-                            sx={{
-                                width: '100%',
-                                backgroundColor: 'white',
-                                '& .MuiLinearProgress-bar': {
-                                    backgroundColor: '#F9A826'
-                                }
-                            }}
-                            nextButton={
-                                <div className='w-5'/>
+                <div className='w-full flex items-center justify-center'>
+                    <MobileStepper
+                        variant="progress"
+                        steps={4}
+                        position="static"
+                        activeStep={activeStep}
+                        sx={{
+                            width: '100%',
+                            backgroundColor: 'white',
+                            '& .MuiLinearProgress-bar': {
+                                backgroundColor: '#F9A826'
                             }
-                            backButton={
-                                <div className='w-5'/>
-                            }
-                        />
-                    </div>
-                <div
-                    className='flex flex-col items-center gap-y-8 w-full sm:w-2/3 md:w-1/2 lg:w-1/3'>
-                    <AsyncPaginate
-                        classNames={{
-                            control: () =>
-                                'border border-gray-300 w-full rounded-md h-[48px] mt-1 text-sm px-3 mr-2 ',
-                            container: () => 'text-sm rounded w-full',
-                            menu: () => 'bg-withe border py-2',
-                            option: ({isSelected}) =>
-                                isSelected ? "dark:bg-base-content dark:text-base-200 bg-gray-400 text-gray-50 px-4 py-2" : "px-4 py-2",
                         }}
-                        value={''}
-                        onChange={(e: any) => console.log(e.target.value)}
-                        unstyled
-                        placeholder="Field of proficiency"
-                        loadOptions={loadOptions}/>
-                    <AsyncPaginate
-                        classNames={{
-                            control: () =>
-                                'border border-gray-300 w-full rounded-md h-[48px] mt-1 text-sm px-3 mr-2',
-                            container: () => 'text-sm rounded w-full',
-                            menu: () => 'bg-withe border py-2',
-                            option: ({isSelected}) =>
-                                isSelected ? "dark:bg-base-content dark:text-base-200 bg-gray-400 text-gray-50 px-4 py-2" : "px-4 py-2",
-                        }}
-                        value={''}
-                        onChange={(e: any) => console.log(e.target.value)}
-                        unstyled
-                        placeholder="Dream job"
-                        loadOptions={loadOptions}/>
-
+                        nextButton={
+                            <div className='w-5'/>
+                        }
+                        backButton={
+                            <div className='w-5'/>
+                        }
+                    />
                 </div>
-                <Link href='/'
+                {renderCurrentStepComponent()}
+                <Link onClick={() => setActiveStep(1)} href='/'
                       className='btn btn-warning w-full max-w-xs bg-[#F9A826] text-white rounded-md shadow-md mt-6 py-2 px-3'>
                     Next
                 </Link>
