5b0536e0c179e829e0fc09522247e95f64032672
hosseinra1 <nh.ra1802@outlook.com>
2024-05-13 00:12:36 +0330

    91: remove config from next.config.mjs.

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
         output: 'standalone',
-        async rewrites() {
-                return [
-                        {
-                                source: '/api/:path*',
-                                destination: 'http://api-dev.linchpino.com:8081/:path*',
-                        },
-                ]
-        },
 };
 
 export default nextConfig;
