b66970a60c00647e0f53bf9c11190f6f1cb5f07e
HosseinRA1 <34892391+HosseinRA1@users.noreply.github.com>
2024-05-21 16:39:15 +0330

    83 :interview step 2 api (#13)

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,27 +1,40 @@
+'use client'
+import { SubmitHandler,useForm } from 'react-hook-form';
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Link from "next/link";
 
+interface SignInForm {
+    email: string;
+    password: string;
+}
+
 export default function SignIn() {
+    const { register, handleSubmit } = useForm<SignInForm>();
+
+    const onSubmit: SubmitHandler<SignInForm> = data => {
+        console.log(data);
+    };
+
     return (
         <>
             <Header/>
             <div className='bg-white container pb-5 lg:pb-0'>
-                <div className="flex flex-col items-center justify-center gap-y-8 mt-14">
+                <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col items-center justify-center gap-y-8 mt-14">
                     <h1 className='text-black text-3xl'>Sign In</h1>
                     <label className="form-control w-full max-w-xs">
                         <div className="label">
                             <span className="label-text">Email Address:</span>
                         </div>
                         <input type="text" placeholder="Your registered email address"
-                               className="input input-bordered w-full max-w-xs bg-white"/>
+                               className="input input-bordered w-full max-w-xs bg-white" {...register('email')} />
                     </label>
                     <label className="form-control w-full max-w-xs">
                         <div className="label">
                             <span className="label-text">Password:</span>
                         </div>
                         <input type="password" placeholder="********"
-                               className="input input-bordered w-full max-w-xs bg-white"/>
+                               className="input input-bordered w-full max-w-xs bg-white" {...register('password')} />
                     </label>
                     <Link href='/'
                           className='btn btn-warning w-full max-w-xs bg-[#F9A826] text-white rounded-md shadow-md mt-6 py-2 px-3'>
@@ -36,7 +49,7 @@ export default function SignIn() {
                             Forgot Password
                         </Link>
                     </div>
-                </div>
+                </form>
             </div>
             <Footer/>
         </>
--- src/containers/home/JobSearchSection.tsx
+++ src/containers/home/JobSearchSection.tsx
@@ -1,12 +1,12 @@
 "use client"
-import React, { useState, useRef } from "react";
+import React, {useState, useRef} from "react";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
-import { AsyncPaginate } from "react-select-async-paginate";
-import { BASE_URL_API } from "@/utils/system";
+import {AsyncPaginate} from "react-select-async-paginate";
+import {BASE_URL_API} from "@/utils/system";
 import axios from "axios";
-import { empty } from "@/utils/helper";
-import { useUpdateEffect } from "react-use";
+import {empty} from "@/utils/helper";
+import {useUpdateEffect} from "react-use";
+import Link from "next/link";
 
 interface Job {
     id: number;
@@ -19,18 +19,17 @@ interface Interview {
 }
 
 export default function JobSearchSection() {
-    const router = useRouter();
     const interViewRef = useRef(null);
     const [jobValue, setJobValue] = useState<Job | null>(null);
     const [interviewValue, setInterviewValue] = useState<Interview | null>(null);
 
     useUpdateEffect(() => {
         if (jobValue && !empty(jobValue)) {
-            loadInterview("",[],{ page: 0 });
+            loadInterview("", [], {page: 0});
         }
     }, [jobValue]);
 
-    const loadJob = async (search: string,loadedOptions:unknown[], { page }: { page: number }): Promise<{
+    const loadJob = async (search: string, loadedOptions: unknown[], {page}: { page: number }): Promise<{
         additional: {
             page: number
         };
@@ -47,7 +46,6 @@ export default function JobSearchSection() {
             value: item.id,
             label: item.title,
         }));
-        console.log(response)
         return {
             options,
             hasMore: !response.data.last,
@@ -57,15 +55,15 @@ export default function JobSearchSection() {
         };
     };
 
-    const loadInterview = async (search: string,loadedOptions:unknown[], { page }: { page: number }): Promise<{
+    const loadInterview = async (search: string, loadedOptions: unknown[], {page}: { page: number }): Promise<{
         additional: {
             page: number
         };
         options: Job[];
         hasMore: boolean
-    }>=>{
+    }> => {
         if (!jobValue) { // @ts-ignore
-            return Promise.resolve({ options: [], additional: { page: page + 1 } });
+            return Promise.resolve({options: [], additional: {page: page + 1}});
         }
         try {
             // @ts-ignore
@@ -81,13 +79,13 @@ export default function JobSearchSection() {
 
             if (options.length > 0) {
                 setInterviewValue(options[0]);
-            }else {
+            } else {
                 //@ts-ignore
                 setInterviewValue(null);
             }
             return {
                 options,
-                hasMore : !response.data.last,
+                hasMore: !response.data.last,
                 additional: {
                     page: page + 1,
                 },
@@ -95,7 +93,7 @@ export default function JobSearchSection() {
         } catch (error) {
             console.error("Error loading interviews:", error);
             // @ts-ignore
-            return Promise.resolve({ options: [], additional: { page: page + 1 } });
+            return Promise.resolve({options: [], additional: {page: page + 1}});
         }
     };
 
@@ -103,7 +101,7 @@ export default function JobSearchSection() {
         <div className="flex flex-col items-center justify-center text-black mt-8 md:mt-0">
             <Image
                 className="hidden md:flex"
-                style={{ position: "absolute", top: 25, left: 0 }}
+                style={{position: "absolute", top: 25, left: 0}}
                 src="/home/HomeCloudTwo.svg"
                 alt="logo"
                 width={350}
@@ -111,20 +109,20 @@ export default function JobSearchSection() {
             />
             <Image
                 className="hidden md:flex"
-                style={{ position: "absolute", top: 10, right: 200 }}
+                style={{position: "absolute", top: 10, right: 200}}
                 src="/home/HomeCloudOne.svg"
                 alt="logo"
                 width={260}
                 height={119}
             />
             <div className="hidden md:flex justify-between relative z-5">
-                <Image src="/home/HomeCloudThree.svg" alt="logo" width={312} height={61} />
-                <Image className="mt-5" src="/home/HomeCloudThree.svg" alt="logo" width={700} height={150} />
+                <Image src="/home/HomeCloudThree.svg" alt="logo" width={312} height={61}/>
+                <Image className="mt-5" src="/home/HomeCloudThree.svg" alt="logo" width={700} height={150}/>
             </div>
             <div className="flex flex-col items-center justify-between relative">
                 <h2 className="relative z-5 text-center md:ml-[40%] ">
                     Empower Your Job Search
-                    <br />
+                    <br/>
                     Participate in a Free Mock Interview with Professionals
                 </h2>
                 <div className="flex flex-col justify-between items-center">
@@ -136,13 +134,14 @@ export default function JobSearchSection() {
                         height={180}
                     />
                 </div>
-                <div className="flex flex-col items-center text-left backdrop-blur bg-black/5 px-5 py-10 mt-4 sm:absolute sm:top-[15%] md:top-0 md:left-[-3rem] w-72 border-2 border-[#F9A826] rounded-lg gap-y-8">
+                <div
+                    className="flex flex-col items-center text-left backdrop-blur bg-black/5 px-5 py-10 mt-4 sm:absolute sm:top-[15%] md:top-0 md:left-[-3rem] w-72 border-2 border-[#F9A826] rounded-lg gap-y-8">
                     <AsyncPaginate
                         classNames={{
                             control: () => " border border-gray-300 w-full rounded-md h-[48px] mt-1 text-sm px-3 mr-2",
                             container: () => "text-sm rounded w-full text-[#000000]",
                             menu: () => " bg-gray-100 rounded border py-2 ",
-                            option: ({ isSelected, isFocused }) =>
+                            option: ({isSelected, isFocused}) =>
                                 isSelected
                                     ? "dark:bg-base-content dark:text-base-200 bg-gray-400 text-gray-50 px-4 py-2"
                                     : isFocused
@@ -168,7 +167,7 @@ export default function JobSearchSection() {
                             control: () => " border border-gray-300 w-full rounded-md h-[48px] mt-1 text-sm px-3 mr-2",
                             container: () => "text-sm rounded w-full text-[#000000]",
                             menu: () => " bg-gray-100 rounded border py-2 ",
-                            option: ({ isSelected, isFocused }) =>
+                            option: ({isSelected, isFocused}) =>
                                 isSelected
                                     ? "dark:bg-base-content dark:text-base-200 bg-gray-400 text-gray-50 px-4 py-2"
                                     : isFocused
@@ -187,10 +186,21 @@ export default function JobSearchSection() {
                     />
                     <button
                         disabled={!jobValue || !interviewValue}
-                        onClick={() => router.push("/schedule-inteview")}
+                        // onClick={() => router.push("/schedule-inteview",{interviewTypeId : interviewValue.value})}
                         className="btn btn-sm w-2/3 border-none px-2 bg-[#3F3D56] text-[#F9A826] rounded-md shadow-md text-xs"
                     >
-                        Schedule the interview
+                        <Link
+                            href={{
+                                pathname: '/schedule-inteview',
+                                query: {
+                                    //@ts-ignore
+                                    interviewTypeId: interviewValue?.value
+                                },
+                            }}
+                        >
+
+                            Schedule the interview
+                        </Link>
                     </button>
                 </div>
             </div>
