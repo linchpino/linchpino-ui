f5378aa82ebb2af36cf3e006a7ae9472d273c20d
hosseinra1 <nh.ra1802@outlook.com>
2024-06-09 14:31:19 +0330

    55: Handle error from response and change password pattern.

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,35 +1,33 @@
 'use client'
-import { SubmitHandler, useForm } from "react-hook-form";
-import { zodResolver } from '@hookform/resolvers/zod';
+import {SubmitHandler, useForm} from "react-hook-form";
+import {zodResolver} from '@hookform/resolvers/zod';
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Link from "next/link";
-import { z } from "zod";
+import {z} from "zod";
 import axios from "axios";
-import { Bounce, toast, ToastContainer } from "react-toastify";
-import { ClipLoader } from 'react-spinners';
-import { useState } from 'react';
+import {Bounce, toast, ToastContainer} from "react-toastify";
+import {ClipLoader} from 'react-spinners';
+import {useState} from 'react';
 import 'react-toastify/dist/ReactToastify.css';
-import { BASE_URL_API } from "@/utils/system";
-import CustomToast, { toastError, toastSuccess } from "@/components/CustomToast";
+import {BASE_URL_API} from "@/utils/system";
+import CustomToast, {toastError, toastSuccess} from "@/components/CustomToast";
 
-const passwordPattern = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-
+const passwordPattern = /^(?=.*[A-Za-z\d@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
 const schema = z.object({
     firstName: z.string().min(1, "First name is required"),
     lastName: z.string().min(1, "Last name is required"),
     email: z.string().email("Invalid email address"),
-    password: z.string().min(6, "Password must contain at least 6 character(s) must include at least one letter, one number, and one special character").regex(passwordPattern, "Password must include at least one letter, one number, and one special character"),
-    repeat_password: z.string().min(6, "Re-Password must contain at least 6 character(s) must include at least one letter, one number, and one special character").regex(passwordPattern, "Re-Password must include at least one letter, one number, and one special character"),
+    password: z.string().min(6, "Password must contain at least 6 character(s)").regex(passwordPattern, "Password must include at least one letter, one number, or one special character"),
+    repeat_password: z.string().min(6, "Re-Password must contain at least 6 character(s)").regex(passwordPattern, "Re-Password must include at least one letter, one number, or one special character"),
 }).refine((data) => data.password === data.repeat_password, {
     message: "Passwords don't match",
     path: ["repeat_password"],
 });
-
 type SignUpFields = z.infer<typeof schema>;
 
 export default function SignUp() {
-    const { register, handleSubmit, formState: { errors } } = useForm<SignUpFields>({
+    const {register, handleSubmit, formState: {errors}} = useForm<SignUpFields>({
         resolver: zodResolver(schema)
     });
     const [isLoading, setIsLoading] = useState(false);
@@ -38,19 +36,18 @@ export default function SignUp() {
         setIsLoading(true);
         try {
             const response = await axios.post(`${BASE_URL_API}accounts`, data);
-            toastSuccess({ message: 'Registration successful!' });
+            toastSuccess({message: 'Registration successful!'});
             return response.data;
         } catch (error) {
+            console.log(error)
             if (axios.isAxiosError(error)) {
-                if (error.response?.status === 400 && error.response?.data?.error === "Unique email violation") {
-                    toastError({ message: 'Email is already in use. Please use a different email.'});
-                } else if (error.response?.status === 400 && error.response?.data?.message === "bad request") {
-                    toastError({ message: 'Please enter your information correctly.'});
+                if (error.response?.status === 400 && error.response?.data?.error) {
+                    toastError({message: error.response?.data?.error});
                 } else if (error.response?.status === 500) {
-                    toastError({ message: 'An error occurred. Please try again.'});
+                    toastError({message: 'An error occurred. Please try again.'});
                 }
             } else {
-                toastError({ message: 'Registration failed. Please try again.'});
+                toastError({message: 'Registration failed. Please try again.'});
             }
             throw error;
         } finally {
@@ -59,16 +56,16 @@ export default function SignUp() {
     };
 
     const onSubmit: SubmitHandler<SignUpFields> = async (data) => {
-        const { repeat_password, ...dataToSubmit } = data;
+        const {repeat_password, ...dataToSubmit} = data;
         try {
-            await sendSignupForm({ ...dataToSubmit, type: 1 });
+            await sendSignupForm({...dataToSubmit, type: 1});
         } catch (error) {
             console.error('Signup failed', error);
         }
     };
     return (
         <>
-            <Header />
+            <Header/>
             <div className='bg-white container pb-5 lg:pb-0'>
                 <div className="flex flex-col items-center justify-center gap-y-8 mt-14">
                     <h1 className='text-black text-3xl'>Sign Up</h1>
@@ -79,7 +76,7 @@ export default function SignUp() {
                                     <span className="label-text">First Name:</span>
                                 </div>
                                 <input {...register("firstName")} type="text" placeholder="Your first name"
-                                       className="input input-bordered w-full bg-white" />
+                                       className="input input-bordered w-full bg-white"/>
                                 {errors.firstName && (
                                     <div className="text-red-500 text-sm mt-1">{errors.firstName.message}</div>
                                 )}
@@ -89,7 +86,7 @@ export default function SignUp() {
                                     <span className="label-text">Last Name:</span>
                                 </div>
                                 <input {...register("lastName")} type="text" placeholder="Your last name"
-                                       className="input input-bordered w-full bg-white" />
+                                       className="input input-bordered w-full bg-white"/>
                                 {errors.lastName && (
                                     <div className="text-red-500 text-sm mt-1">{errors.lastName.message}</div>
                                 )}
@@ -100,7 +97,7 @@ export default function SignUp() {
                                 <span className="label-text">Email:</span>
                             </div>
                             <input {...register("email")} type="email" placeholder="***@gmail.com"
-                                   className="input input-bordered w-full bg-white" />
+                                   className="input input-bordered w-full bg-white"/>
                             {errors.email && (
                                 <div className="text-red-500 text-sm mt-1">{errors.email.message}</div>
                             )}
@@ -111,7 +108,7 @@ export default function SignUp() {
                                     <span className="label-text">Password:</span>
                                 </div>
                                 <input {...register("password")} type="password" placeholder="********"
-                                       className="input input-bordered w-full bg-white" />
+                                       className="input input-bordered w-full bg-white"/>
                                 {errors.password && (
                                     <div className="text-red-500 text-sm mt-1">{errors.password.message}</div>
                                 )}
@@ -121,7 +118,7 @@ export default function SignUp() {
                                     <span className="label-text">Re-Password:</span>
                                 </div>
                                 <input {...register("repeat_password")} type="password" placeholder="********"
-                                       className="input input-bordered w-full bg-white" />
+                                       className="input input-bordered w-full bg-white"/>
                                 {errors.repeat_password && (
                                     <div className="text-red-500 text-sm mt-1">{errors.repeat_password.message}</div>
                                 )}
@@ -130,7 +127,7 @@ export default function SignUp() {
                         <button type="submit"
                                 className='btn btn-warning w-full max-w-xs bg-[#F9A826] text-white rounded-md shadow-md mt-6 py-2 px-3 self-center'
                                 disabled={isLoading}>
-                            {isLoading ? <ClipLoader size={24} color={"#fff"} /> : 'Register'}
+                            {isLoading ? <ClipLoader size={24} color={"#fff"}/> : 'Register'}
                         </button>
                     </form>
 
@@ -145,8 +142,8 @@ export default function SignUp() {
                     </div>
                 </div>
             </div>
-            <Footer />
-            <ToastContainer />
+            <Footer/>
+            <ToastContainer/>
         </>
     );
 }
