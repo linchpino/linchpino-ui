9089bb0591844b2027b78a21d669cf5c5022e1b8
hosseinra1 <nh.ra1802@outlook.com>
2024-07-21 15:34:18 +0330

    142: Sidebar posiotion bug fixed.

--- src/app/(main)/panel/layout.tsx
+++ src/app/(main)/panel/layout.tsx
@@ -7,7 +7,7 @@ const PanelLayout = ({ children }: { children: ReactNode }) => {
     return(
       <div >
           <Header />
-          <div className='flex flex-col md:flex-row gap-x-8 gap-y-6 md:gap-y-0 container mt-10 relative items-center md:items-start'>
+          <div className='flex flex-col md:flex-row gap-x-8 gap-y-6 md:gap-y-0 container mt-10 relative  items-center md:items-start'>
               <Sidebar />
              <div className='shadow-[0px_10px_25px_-5px_rgba(0,0,0,0.3)] w-full md:w-2/3 rounded-md'>
                  {children}
--- src/containers/panel/Sidebar.tsx
+++ src/containers/panel/Sidebar.tsx
@@ -10,7 +10,7 @@ const Sidebar = () => {
     ];
 
     return (
-        <div className="shadow-[0px_10px_25px_-5px_rgba(0,0,0,0.3)] w-full md:w-1/3 rounded-md p-4 max-h-80 lg:sticky ">
+        <div className="shadow-[0px_10px_25px_-5px_rgba(0,0,0,0.3)] w-full md:w-1/3 rounded-md p-4 max-h-80 lg:sticky top-10">
             <ul>
                 {links.map((link) => (
                     <li key={link.href} className={`${pathname === link.href ? 'bg-[#F9A826] text-white' : ''} p-2 my-3 rounded-md`}>
