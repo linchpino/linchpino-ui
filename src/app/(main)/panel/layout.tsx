d5c64fdf3873037ffe18f854b0feb7ec6caed8ce
hosseinra1 <nh.ra1802@outlook.com>
2024-07-25 17:34:57 +0330

    140: Panel now is responsive.

--- src/app/(main)/panel/layout.tsx
+++ src/app/(main)/panel/layout.tsx
@@ -7,9 +7,9 @@ const PanelLayout = ({ children }: { children: ReactNode }) => {
     return(
       <div>
           <Header />
-          <div className='flex gap-x-8 container mt-10'>
+          <div className='flex gap-x-4 sm:gap-x-8 p-6 sm:container mt-6'>
               <Sidebar />
-             <div className='shadow-[0px_10px_25px_-5px_rgba(0,0,0,0.3)] w-2/3 rounded-md'>
+             <div className='shadow-[0px_10px_25px_-5px_rgba(0,0,0,0.3)] w-[85%] md:w-4/5 rounded-md'>
                  {children}
              </div>
           </div>
