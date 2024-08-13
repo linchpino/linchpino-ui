b8217b278deeb5dc955bc1768fff5e654b64a5f0
HosseinRa1 <nh.ra1802@outlook.com>
2024-08-13 15:04:08 +0330

    150: Improve UI and responsive design.

---
+++ src/containers/panel/profile/ProfileTimeSlot.tsx
@@ -0,0 +1,197 @@
+import React, {useRef, useState} from "react";
+import DatePicker from "react-multi-date-picker";
+import TimePicker from "react-multi-date-picker/plugins/time_picker";
+import gregorian from "react-date-object/calendars/gregorian";
+import gregorian_en from "react-date-object/locales/gregorian_en";
+import Select from "react-select";
+import "react-multi-date-picker/styles/colors/yellow.css"
+
+const ProfileTimeSlot =() =>{
+    const timeCards = [
+        {id: 1, start: "2024-08-10", duration: "30 min", repeat: "no", days: ["Su", "Mo", "Tu", "We", "Th"]},
+        {id: 2, start: "2024-08-11", duration: "60 min", repeat: "1 week", days: ["Su", "Mo", "Tu", "We", "Th"]},
+        {id: 3, start: "2024-08-13", duration: "15 min", repeat: "1 week", days: ["Su", "We", "Th"]},
+        {id: 4, start: "2024-08-14", duration: "45 min", repeat: "2 week", days: ["Su"]},
+    ]
+    const duration = [
+        {id: 1, time: "15 min"},
+        {id: 2, time: "20 min"},
+        {id: 3, time: "30 min"},
+        {id: 4, time: "45 min"},
+        {id: 5, time: "60 min"},
+    ]
+    const days = [
+        {id: 1, day: "Sun"},
+        {id: 2, day: "Mon"},
+        {id: 3, day: "Tue"},
+        {id: 4, day: "Wed"},
+        {id: 5, day: "Thu"},
+        {id: 6, day: "Fri"},
+        {id: 7, day: "Sat"},
+    ]
+    const options = [
+        {value: 'day', label: 'Day'},
+        {value: 'week', label: 'Week'},
+        {value: 'month', label: 'Month'}
+    ]
+
+    const [selectedStart, setSelectedStart] = useState()
+    const [selectedDuration, setSelectedDuration] = useState("");
+    const [selectedDay, setSelectedDay] = useState("");
+    const [selectedRepeat, setSelectedRepeat] = useState({value:"",label:""});
+
+    const modalRef = useRef<HTMLDialogElement>(null);
+console.log(selectedRepeat)
+    const handleLoginClick = () => {
+        if (modalRef.current) {
+            modalRef.current.showModal();
+        }
+    };
+    return(
+        <>
+            <div className='flex gap-x-2 mt-8'>
+                <h4>Time Slot</h4>
+                <button onClick={handleLoginClick}
+                        className='bg-amber-400 flex items-center justify-center text-[22px] text-white w-6 h-6 rounded-full'>+
+                </button>
+            </div>
+            <dialog ref={modalRef} id="modal"
+                    className="modal">
+                <div className="modal-box max-w-lg bg-white flex flex-col items-center">
+                    <form method="dialog">
+                        <button
+                            className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕
+                        </button>
+                    </form>
+                    <div
+                        className="flex flex-col pb-3 lg:pb-6 items-center justify-center w-full rounded-md mt-2 mb-10 lg:mb-0 container p-3">
+                        <h1 className='text-xl text-center text-[#000]'>Add Time Slot</h1>
+                        <div className='flex flex-col items-center justify-center mt-4 gap-y-5'>
+                            <div className='flex flex-col sm:flex-row items-center w-full gap-y-2 gap-x-2'>
+                                <span className='text-sm'>Start: </span>
+                                <DatePicker
+                                    containerClassName='w-full sm:w-1/2'
+                                    inputClass='profile-calendar w-full'
+                                    className='yellow '
+                                    value={selectedStart}
+                                    //@ts-ignore
+                                    onChange={setSelectedStart}
+                                    calendar={gregorian}
+                                    locale={gregorian_en}
+                                    calendarPosition="bottom-right"
+                                    placeholder='Date'
+                                />
+                                <DatePicker
+                                    containerClassName='w-full sm:w-1/2'
+                                    inputClass='profile-timepicker w-full'
+                                    disableDayPicker
+                                    format="HH:mm:ss"
+                                    placeholder='Time'
+                                    plugins={[
+                                        <TimePicker />
+                                    ]}
+                                    calendar={gregorian}
+                                    locale={gregorian_en}
+                                    calendarPosition="bottom-right"
+                                />
+                            </div>
+                            <div className='flex items-center w-full flex-col sm:flex-row '>
+                                <span className='text-sm'>Duration: </span>
+                                <div className='flex gap-x-2 ml-2 flex-wrap justify-center gap-y-3 mt-2 sm:mt-0'>
+                                    {duration.map(durationItem => {
+                                        return (
+                                            <button onClick={() => setSelectedDuration(durationItem.time)}
+                                                    key={durationItem.id}
+                                                    className={`p-2 min-w-[3.9rem] text-xs rounded-3xl flex items-center justify-center border-[.1px] ${selectedDuration === durationItem.time && 'text-[#F2A926]'} ${selectedDuration === durationItem.time && 'border-[#F2A926]'} `}>
+                                                {durationItem.time}
+                                            </button>
+                                        )
+                                    })}
+                                </div>
+                            </div>
+                            <div className='flex items-center w-full gap-x-2 flex-col sm:flex-row gap-y-3'>
+                                <span className='text-sm'>Repeat every: </span>
+                                <input type="text" placeholder="number of..."
+                                       className="input input-bordered w-full sm:w-1/3 input-sm h-[2.4rem] text-xs bg-white"/>
+                                <Select
+                                    unstyled
+                                    classNames={{
+                                        control: () => "border border-gray-300 w-full rounded-lg h-[20px] text-xs px-3 mr-2",
+                                        container: () => "text-xs rounded w-full sm:w-1/3 text-[#000000]",
+                                        menu: () => "bg-gray-100 rounded border py-2",
+                                        option: ({isSelected, isFocused}) =>
+                                            isSelected
+                                                ? "dark:bg-base-content dark:text-base-200 bg-gray-400 text-gray-50 px-4 py-2"
+                                                : isFocused
+                                                    ? "bg-gray-200 px-4 py-2"
+                                                    : "px-4 py-2",
+                                    }}
+                                    defaultValue={options[0]}
+                                    options={options}
+                                    //@ts-ignore
+                                    onChange={setSelectedRepeat}
+                                />
+                            </div>
+                            {selectedRepeat.value === "week" &&
+                                <div className='flex items-center w-full flex-col sm:flex-row'>
+                                    <span className='text-sm'>On: </span>
+                                    <div className='flex gap-x-2 ml-2 justify-center flex-wrap mt-2 sm:mt-0 gap-y-2'>
+                                        {days.map(daysItem => {
+                                            return (
+                                                <button onClick={() => setSelectedDay(daysItem.day)} key={daysItem.id}
+                                                        className={`p-2 min-w-12 text-xs rounded-3xl flex items-center justify-center border-[.1px] ${selectedDay === daysItem.day && 'text-[#F2A926]'} ${selectedDay === daysItem.day && 'border-[#F2A926]'} `}>
+                                                    {daysItem.day}
+                                                </button>
+                                            )
+                                        })}
+                                    </div>
+                                </div>
+                            }
+                        </div>
+
+                    </div>
+
+                    <button
+                        className={`btn btn-sm w-28 xs:w-36 border-none px-2 bg-[#F9A826] text-[#FFFFFF] rounded-md shadow-md text-xs hover:bg-[#F9A945]`}>
+                        Add
+                    </button>
+                </div>
+            </dialog>
+            <div className='grid grid-cols-1 lg:grid-cols-2 gap-x-6 gap-y-6 mt-4'>
+                {timeCards.map(item => {
+                    return (
+                        <div key={item.id} className="relative bg-white rounded-lg card shadow-xl text-xs lg:text-sm">
+                            <div
+                                className="absolute top-0 left-0 w-full h-1 bg-gradient-to-r from-purple-400 via-pink-500 to-[#F9A826] rounded-t-lg"></div>
+                            <div className="card-body px-4 py-6 flex flex-col">
+                                <div
+                                    className={`flex ${item.repeat === 'no' ? 'flex-col' : 'flex-col md:flex-row'} ${item.repeat === 'no' && 'gap-y-3'} w-full justify-between items-center`}>
+                                    <span>Start: {item.start}</span>
+                                    <span>Duration: {item.duration}</span>
+                                </div>
+                                {item.repeat !== 'no' &&
+                                    <div className='flex md:flex-row flex-col justify-between items-center mt-2 gap-x-2 gap-y-2'>
+                                        <span className='text-sm'>Repeat <span
+                                            className='text-[1rem] text-gray-700'>{item.repeat}</span> in </span>
+                                        <div className='flex flex-wrap gap-x-2 justify-center md:justify-start items-center md:w-[15rem]'>
+                                            {item.days.map(daysItem => {
+                                                return (
+                                                    <div
+                                                        key={daysItem}
+                                                        className='flex items-center justify-center text-sm font-light text-white w-8 h-8 rounded-full bg-[#F9A826]'>
+                                                        {daysItem}
+                                                    </div>
+                                                )
+                                            })}
+                                        </div>
+                                    </div>
+                                }
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        </>
+    )
+}
+export default ProfileTimeSlot
