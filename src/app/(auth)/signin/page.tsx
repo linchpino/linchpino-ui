b92e9fb1c0351b7aafecf699572597884de6639e
HosseinRa1 <nh.ra1802@outlook.com>
2024-08-15 21:53:25 +0330

    68: Linkedin details hard code for test.

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -102,7 +102,7 @@ export default function SignIn() {
 
         if (code) {
             setIsLoadingLinkedin(true);
-            // fetchAccessToken(code);
+            fetchAccessToken(code);
         }
     }, [searchParams]);
 
