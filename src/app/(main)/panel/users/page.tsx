118a627bee75df06587ee229897deaea450281cb
HosseinRa1 <nh.ra1802@outlook.com>
2024-10-06 02:37:13 +0330

    223:Change accounts status and role by admin.

--- src/app/(main)/panel/users/page.tsx
+++ src/app/(main)/panel/users/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, {useState, useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useMutation, useQuery, useQueryClient} from '@tanstack/react-query';
 import axios from 'axios';
 import Select from 'react-select';
@@ -9,16 +9,42 @@ import Spinner from "@/components/Spinner";
 import ProtectedPage from "@/app/(main)/panel/ProtectedPage";
 import {textWithTooltip} from "@/utils/helper";
 import {toastError, toastSuccess} from "@/components/CustomToast";
-
 interface UserType {
     id: number;
     firstName: string;
     lastName: string;
     email: string;
     roles: string[];
-    status: number;
+    status: string;
+}
+interface UsersData {
+    totalPages: number;
+    last: boolean;
+    content: UserType[];
 }
 
+type StatusKeys = 'ACTIVATED' | 'DEACTIVATED';
+
+const statusTextToNumber: Record<StatusKeys, number> = {
+    ACTIVATED: 1,
+    DEACTIVATED: 2,
+};
+
+const statusNumberToText = {
+    1: "ACTIVATED",
+    2: "DEACTIVATED"
+};
+
+const roleToNumberMap: {[key: string]: number} = {
+    "GUEST": 1,
+    "JOBSEEKER": 2,
+    "MENTOR": 3,
+    "ADMIN": 4
+};
+
+const mapRolesToNumbers = (roles: string[]): number[] => {
+    return roles.map(role => roleToNumberMap[role] || 0);
+};
 
 const fetchUsers = async (token: string | null, page: number, name: string, role: number | null): Promise<{
     content: UserType[],
@@ -27,67 +53,49 @@ const fetchUsers = async (token: string | null, page: number, name: string, role
 }> => {
     const params: any = {
         page,
-        sort: 'firstName,desc',
+        sort: 'firstName,asc',
         size: 10
     };
-    if (name) {
-        params.name = name;
-    }
-    if (role !== null) {
-        params.role = role;
-    }
+    if (name) params.name = name;
+    if (role !== null) params.role = role;
+
     const {data} = await axios.get(`${BASE_URL_API}admin/accounts/search`, {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
+        headers: { Authorization: `Bearer ${token}` },
         params,
     });
     return data;
 };
+
 const updateUser = async (token: string | null, updatedData: {
     accountId: number,
     roles: number[],
     status: number
 }) => {
     await axios.put(`${BASE_URL_API}admin/accounts/update`, updatedData, {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        }
+        headers: { Authorization: `Bearer ${token}` }
     });
 };
 
 const User = () => {
     const queryClient = useQueryClient();
-    const {token,} = useStore(state => ({
-        token: state.token,
-    }));
-    const roleOptions = [
-        {value: "", label: "All"},
-        {value: 1, label: "GUEST"},
-        {value: 2, label: "JOBSEEKER"},
-        {value: 3, label: "MENTOR"},
-        {value: 4, label: "ADMIN"}
-    ];
-    const statusOptions = [
-        {value: 1, label: "Active"},
-        {value: 2, label: "Deactivate"}
-    ];
-    const [currentPage, setCurrentPage] = useState(0);
+    const {token} = useStore(state => ({ token: state.token }));
     const [itemsPerPage] = useState(10);
+
+    const [currentPage, setCurrentPage] = useState(0);
     const [searchTerm, setSearchTerm] = useState('');
     const [selectedRole, setSelectedRole] = useState<number | null>(null);
     const [isLastPage, setIsLastPage] = useState(false);
 
-    const {data: usersData, isLoading} = useQuery({
+    const {data: usersData, isLoading} = useQuery<UsersData>({
         queryKey: ['users', currentPage, searchTerm, selectedRole],
         queryFn: () => fetchUsers(token, currentPage, searchTerm, selectedRole),
         //@ts-ignore
-        onSuccess: (data) => {
+        onSuccess: (data:any) => {
             setIsLastPage(data.last);
         },
-        onError: (error: any) => {
+        onError: (error: unknown) => {
             console.error('Failed to fetch users:', error);
-        }
+        },
     });
 
     const mutation = useMutation({
@@ -95,52 +103,46 @@ const User = () => {
             accountId: number,
             roles: number[],
             status: number
-        }) => updateUser(token, {accountId:updatedData.accountId, roles: updatedData.roles, status: updatedData.status}),
+        }) => updateUser(token, updatedData),
         onSuccess: () => {
-            toastSuccess({message: 'User updated successfully!'});
-            queryClient.invalidateQueries({queryKey: ['users']});
+            toastSuccess({ message: 'User updated successfully!' });
+            queryClient.invalidateQueries({ queryKey: ['users'] });
         },
-        onError: (error: any) => {
+        onError: (error) => {
             console.error('Failed to update user:', error);
-            toastError({message: 'Error updating user.'});
+            toastError({ message: 'Error updating user.' });
         }
     });
-    const handleUserUpdate = (accountId: number, roles: (number)[], status: number) => {
-        mutation.mutate({accountId, roles, status});
+
+    const handleUserUpdate = (accountId: number, roles: string[], status: string) => {
+        const roleNumbers = mapRolesToNumbers(roles);
+        const statusNumber = statusTextToNumber[status as StatusKeys];
+        mutation.mutate({
+            accountId,
+            roles: roleNumbers,
+            status: statusNumber
+        });
     };
 
     const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setSearchTerm(e.target.value);
         setCurrentPage(0);
     };
+
     const handleRoleChange = (selectedOption: any) => {
         setSelectedRole(selectedOption ? selectedOption.value : null);
         setCurrentPage(0);
     };
-
     useEffect(() => {
+        // @ts-ignore
         if (isLastPage && currentPage > usersData?.totalPages) {
+            // @ts-ignore
             setCurrentPage(usersData?.totalPages);
         }
     }, [isLastPage, currentPage, usersData]);
 
+    // @ts-ignore
     const totalPages = usersData?.totalPages || 0;
-    const ToggleSwitch = ({isActive, onToggle}: { isActive: boolean, onToggle: () => void }) => (
-        <label className="relative inline-flex items-center cursor-pointer">
-            <input
-                type="checkbox"
-                checked={isActive}
-                onChange={onToggle}
-                className="sr-only peer"
-            />
-            <div
-                className={`w-11 h-6 rounded-full ${isActive ? 'bg-green-500' : 'bg-red-500'} peer-checked:bg-green-500 peer-focus:ring-4 peer-focus:ring-blue-300`}
-            />
-            <span
-                className={`absolute left-1 top-[2px] w-5 h-5 bg-white rounded-full transition-transform duration-200 transform ${isActive ? 'translate-x-full' : ''}`}
-            />
-        </label>
-    );
     return (
         <ProtectedPage>
             <div className="mx-auto">
@@ -155,98 +157,106 @@ const User = () => {
                             className="input input-bordered border-[.1px] rounded-md text-gray-900 border-gray-300 w-full max-w-xs h-10 text-sm bg-white"
                         />
                         <Select
-                            options={roleOptions}
+                            options={[
+                                {value: "", label: "All"},
+                                {value: 1, label: "GUEST"},
+                                {value: 2, label: "JOBSEEKER"},
+                                {value: 3, label: "MENTOR"},
+                                {value: 4, label: "ADMIN"}
+                            ]}
                             isClearable
+                            unstyled
                             placeholder="Filter by role"
                             onChange={handleRoleChange}
                             classNames={{
-                                control: () => "border border-gray-50 w-full rounded-lg h-8 text-sm px-1 text-gray-100",
-                                container: () => "text-sm border-gray-50 rounded-lg w-full text-gray-500 text-left",
-                                menu: () => "bg-gray-100 rounded border py-2 rounded-lg ",
-                                option: ({isSelected, isFocused}) => isSelected
-                                    ? "dark:bg-base-content dark:text-base-200 bg-gray-400 text-gray-50 px-4 py-2"
-                                    : isFocused
-                                        ? "bg-gray-200 px-4 py-2"
-                                        : "px-4 py-2",
+                                control: () => "border border-gray-300 w-full rounded-md h-[40px] text-sm px-3 mr-2",
+                                container: () => "text-sm rounded w-full",
+                                menu: () => "bg-gray-100 rounded border py-2",
+                                option: ({isSelected, isFocused}) =>
+                                    isSelected
+                                        ? "bg-gray-400 text-gray-50 px-4 py-2"
+                                        : isFocused
+                                            ? "bg-gray-200 px-4 py-2 text-gray-700 "
+                                            : "px-4 py-2 text-gray-500 ",
                             }}
                         />
                     </div>
                 </div>
+
                 <div className="overflow-x-auto">
                     {isLoading ? (
                         <div className="flex justify-center items-center">
-                            <Spinner loading={isLoading}/>
+                            <Spinner loading={isLoading} />
                         </div>
                     ) : (
                         <table className="table w-full mt-4">
                             <thead>
                             <tr className='text-[.9rem] font-medium border-b-0 bg-[#111B47] text-white h-16'>
-                                <th className="w-12 rounded-tr-none rounded-tl-xl">#</th>
+                                <th className='rounded-tr-none rounded-tl-xl '>#</th>
                                 <th>Name</th>
                                 <th>Email</th>
                                 <th>Status</th>
-                                <th className='w-20 rounded-tl-none rounded-tr-xl'>Role</th>
+                                <th className='rounded-tl-none rounded-tr-xl w-60'>Role</th>
                             </tr>
                             </thead>
                             <tbody>
-                            {usersData?.content?.map((user: UserType, index: number) => {
-                                const isActive = user.status === 1;
-                                const fullName = `${user.firstName} ${user.lastName}`;
-                                return (
-                                    <tr key={user.id}
-                                        className={`${index % 2 === 0 ? 'bg-gray-100 text-[#111B47]' : 'bg-white text-[#111B47]'}`}>
-                                        <td>{currentPage * itemsPerPage + index + 1}</td>
-                                        <td>{textWithTooltip(fullName)}</td>
-                                        <td>{textWithTooltip(user.email)}</td>
-                                        <td>
-                                            <input
-                                                data-theme='light'
-                                                type="checkbox"
-                                                className="toggle toggle-success"
-                                                defaultChecked={isActive}
-                                                onChange={() => handleUserUpdate(user.id, user.roles.map(role => role), isActive ? 1 : 2)}
-                                            />
-                                        </td>
-                                        <td>
-                                            <select
-                                                data-theme='light'
-                                                defaultValue={user.roles[0]}
-                                                className="select select-bordered w-full"
-                                                onChange={(e) => handleUserUpdate(user.id, [parseInt(e.target.value)], user.status)}
-                                            >
-                                                {roleOptions.map((option) => (
-                                                    <option key={option.value} value={option.value}>
-                                                        {option.label}
-                                                    </option>
-                                                ))}
-                                            </select>
-                                        </td>
-
-                                    </tr>
-                                );
-                            })}
+                            {/*@ts-ignore*/}
+                            {usersData?.content?.map((user, index) => (
+                                <tr key={user.id} className={`${index % 2 === 0 ? 'bg-gray-100 text-[#111B47]' : 'bg-white text-[#111B47]'}`}>
+                                    <td>{currentPage * 10 + index + 1}</td>
+                                    <td>{textWithTooltip(`${user.firstName} ${user.lastName}`)}</td>
+                                    <td>{textWithTooltip(user.email)}</td>
+                                    <td>
+                                        <input
+                                            data-theme="light"
+                                            type="checkbox"
+                                            className="toggle toggle-success toggle-sm"
+                                            checked={user.status === "ACTIVATED"}
+                                            onChange={() => handleUserUpdate(user.id, user.roles, user.status === "ACTIVATED" ? "DEACTIVATED" : "ACTIVATED")}
+                                        />
+                                    </td>
+                                    <td>
+                                        <Select
+                                            isMulti
+                                            options={[
+                                                { value: "GUEST", label: "GUEST" },
+                                                { value: "JOBSEEKER", label: "JOBSEEKER" },
+                                                { value: "MENTOR", label: "MENTOR" },
+                                                { value: "ADMIN", label: "ADMIN" }
+                                            ]}
+                                            //@ts-ignore
+                                            menuPortalTarget={document.body}
+                                            styles={{
+                                                menuPortal: base => ({ ...base, zIndex: 9999 })
+                                            }}
+                                            //@ts-ignore
+                                            defaultValue={user.roles.map(role => ({ value: role, label: role }))}
+                                            onChange={(selectedOptions) => handleUserUpdate(user.id, selectedOptions.map(opt => opt.value), user.status)}
+                                        />
+                                    </td>
+                                </tr>
+                            ))}
                             </tbody>
                         </table>
                     )}
                 </div>
-
-                {!isLoading && (
-                    <div className="flex justify-center mt-4">
-                        <div className="flex gap-x-2">
-                            {[...Array(totalPages)].map((_, index) => (
-                                <button
-                                    key={index}
-                                    className={`btn  ${currentPage === index ? 'bg-[#111B47] border-none text-white' : 'hover:text-white text-[#111B47] border-[#111B47] border-[.1px] bg-white'}`}
-                                    onClick={() => setCurrentPage(index)}
-                                    disabled={isLastPage && index + 1 > totalPages}
-                                >
-                                    {index + 1}
-                                </button>
-                            ))}
-                        </div>
-                    </div>
-                )}
             </div>
+            {!isLoading && (
+                <div className="flex justify-center mt-4">
+                    <div className="flex gap-x-2">
+                        {[...Array(totalPages)].map((_, index) => (
+                            <button
+                                key={index}
+                                className={`btn  ${currentPage === index ? 'bg-[#111B47] border-none text-white' : 'hover:text-white text-[#111B47] border-[#111B47] border-[.1px] bg-white'}`}
+                                onClick={() => setCurrentPage(index)}
+                                disabled={isLastPage && index + 1 > totalPages}
+                            >
+                                {index + 1}
+                            </button>
+                        ))}
+                    </div>
+                </div>
+            )}
         </ProtectedPage>
     );
 };
